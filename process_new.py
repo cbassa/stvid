042a68476f0c23985010693b8649250d296d763f
Daniel Ekman <knegge@gmail.com>
2022-09-06 08:28:41 +0200

    Batch process arg, exit when done
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- process_new.py
+++ process_new.py
@@ -40,6 +40,10 @@ if __name__ == "__main__":
                              metavar="DIR",
                              dest="file_dir",
                              default=".")
+    conf_parser.add_argument("-b",
+                             "--batch",
+                             help="Batch process observations, exit when done.",
+                             action="store_true")
     args = conf_parser.parse_args()
     
     # Read configuration file
@@ -187,6 +191,8 @@ if __name__ == "__main__":
 
         # Sleep
         try:
+            if(args.batch):
+                sys.exit()
             print("File queue empty, waiting for new files...\r", end = "")
             time.sleep(10)
         except KeyboardInterrupt:
