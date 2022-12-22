42540252818709ffb721fdd74c3de5240ab4031f
Cees Bassa <cbassa@gmail.com>
2022-12-22 14:35:06 +0100

    Add threaded processing

--- process.py
+++ process.py
@@ -14,12 +14,161 @@ import warnings
 
 from termcolor import colored
 
+import multiprocessing as mp
+
 from stvid import calibration
 from stvid.fourframe import FourFrame
 from stvid.fourframe import Observation
 from stvid.fourframe import AstrometricCatalog
 
 from astropy.utils.exceptions import AstropyWarning
+
+def chunk_list(l, n):
+    o = []
+    for i in range(0, len(l), n):
+        o.append(l[i:i + n])
+    return o
+
+def process_loop(fname):
+    """
+    Thread to process satobs FourFrame FITS files in a multi-thread compatible manner
+    """
+
+    # File root
+    froot = os.path.splitext(fname)[0]
+    
+    # Find stars
+    if not os.path.exists(f"{froot}_stars.cat"):
+        scat = calibration.generate_star_catalog(fname)
+    else:
+        scat = calibration.read_star_catalog(fname)
+
+    # Calibrate
+    screenoutput = None
+    if not os.path.exists(f"{froot}_calib.wcs"):
+        w, rmsx, rmsy, nused, is_calibrated = calibration.calibrate(fname, cfg, acat, scat, wref, tref)
+
+        # Attempt plate solve
+#        if not is_calibrated and scat.nstars > nstarsmin:
+#            print(colored(f"Computing astrometric calibration for {fname}", "yellow"))
+#            wtmp, ttmp = calibration.plate_solve(fname, cfg, calfname)
+            
+#            # Retry calibration
+#            if wtmp is not None:
+#                wref, tref = wtmp, ttmp
+#                w, rmsx, rmsy, nused, is_calibrated = calibration.calibrate(fname, cfg, acat, scat, wref, tref)
+
+        # Log output
+        output = f"{os.path.basename(fname)} {w.wcs.crval[0]:10.6f} {w.wcs.crval[1]:10.6f} {rmsx:6.2f} {rmsy:6.2f} {nused}/{scat.nstars}"
+        if is_calibrated:
+            color = "green"
+        else:
+            color = "red"
+        screenoutput = colored(output, color)
+
+    # Skip if png exists
+    if os.path.exists(f"{froot}_0.png"):
+        return
+        
+    # Read Fourframe
+    ff = FourFrame(fname, cfg)
+        
+    # Generate predictions
+    predictions = ff.generate_satellite_predictions(cfg)
+
+    # Find tracks
+    if ff.is_calibrated():
+        tracks = ff.find_tracks_by_hough3d(cfg)
+    else:
+        tracks = []
+
+    # Output dictionary
+    output_dict = {"site_id": ff.site_id,
+                   "latitude": ff.lat,
+                   "longitude": ff.lon,
+                   "height": ff.height,
+                   "observer": ff.observer}
+    
+    # Loop over tracks
+    ident_dicts = []
+    obs = []
+    satno = 90000
+    for t in tracks:
+        # Identify
+        ident, is_identified = t.identify(predictions, satno, "22 500A", None, cfg, abbrevs, tlefiles)
+        if not is_identified:
+            satno += 1
+
+        # Identification dictionary
+        ident_dict = {"satno": ident.satno,
+                      "cospar": ident.cospar,
+                      "tlefile": ident.tlefile,
+                      "catalogname": ident.catalogname}
+
+        # Save track
+        t.save(f"{ff.froot}_{ident.satno:05d}_{ident.catalogname}.csv", ff)
+
+        # Measure single position
+        m = t.measure_single_position(ff)
+        iod_line = m.to_iod_line(ff, ident)
+
+        # Measure multiple position
+        ms = t.measure_multiple_positions(ff)
+        iod_lines = [mt.to_iod_line(ff, ident) for mt in ms]
+
+        # Add to dictionary
+        single_measurement = {"time": m.t.isot,
+                              "ra": float(m.ra),
+                              "dec": float(m.dec),
+                              "drxdt": float(m.drxdt),
+                              "drydt": float(m.drydt)}
+        multiple_measurements = [{"time": mt.t.isot,
+                                  "ra": float(mt.ra),
+                                  "dec": float(mt.dec),
+                                  "drxdt": float(mt.drxdt),
+                                  "drydt": float(mt.drydt)} for mt in ms]
+        ident_dict["measurement"] = single_measurement
+        ident_dict["iod_line"] = iod_line
+        ident_dict["measurements"] = multiple_measurements
+        ident_dict["iod_lines"] = [line for line in iod_lines]
+        ident_dicts.append(ident_dict)
+
+        # Store observation
+        obs.append(Observation(ident.satno, ident.catalogname, iod_line, iod_lines))
+
+        
+    # Store output
+    if ident_dicts is not []:
+        output_dict["satellites"] = ident_dicts
+
+        with open(f"{ff.froot}_data.yaml", "w") as fp:
+            yaml.dump(output_dict, fp, sort_keys=False)
+        
+    # Write observations
+    screenoutput_idents = []
+    for o in obs:
+        # Open file
+        outfname = f"{ff.froot}_{o.satno:05d}_{o.catalogname}.dat"
+        with open(outfname, "w") as fp:
+            fp.write(f"{o.iod_line}\n")
+        outfname = f"{ff.froot}_{o.satno:05d}_{o.catalogname}_m.dat"
+        with open(outfname, "w") as fp:
+            for iod_line in o.iod_lines:
+                fp.write(f"{iod_line}\n")
+        if o.catalogname == "catalog":
+            color = "grey"
+        elif o.catalogname == "classfd":
+            color = "blue"
+        elif o.catalogname == "unid":
+            color = "magenta"
+        screenoutput_idents.append(colored(o.iod_line, color))
+
+    # Generate plots
+    ff.diagnostic_plot(predictions, None, None, cfg)
+    for track, o in zip(tracks, obs):
+        ff.diagnostic_plot(predictions, track, o, cfg)
+
+    return (screenoutput, screenoutput_idents)
 
 if __name__ == "__main__":
     # Read commandline options
@@ -64,12 +213,21 @@ if __name__ == "__main__":
 
     # Read astrometric catalog
     acat = AstrometricCatalog(cfg.getfloat("Astrometry", "max_magnitude"))
-            
+
+    # Get number of CPUs for multiprocessing
+    if cfg.has_option("LineDetection", "cpu_count"):
+        cpu_count = cfg.getint("LineDetection", "cpu_count")
+    else:
+        cpu_count = mp.cpu_count()
+    print(f"Processing with {cpu_count} threads")
+        
     # Start processing loop
     while True:
-       # Get files
-        fnames = sorted(glob.glob(os.path.join(args.file_dir, "2*.fits")))
-
+        # Get unprocessed files
+        fitsfnames = sorted(glob.glob(os.path.join(args.file_dir, "2*.fits")))
+        froots = [os.path.splitext(fitsname)[0] for fitsname in fitsfnames]
+        fnames = [f"{froot}.fits" for froot in froots if not os.path.exists(f"{froot}_0.png")]
+        
         # Create reference calibration file
         calfname = os.path.join(args.file_dir, "test.fits")
         if not os.path.exists(calfname):
@@ -104,145 +262,26 @@ if __name__ == "__main__":
         if not solved:
             print("Plate solving failed, exiting...")
             sys.exit()
-            
-        # Loop over files
-        for fname in fnames:
-            # File root
-            froot = os.path.splitext(fname)[0]
-            
-            # Find stars
-            if not os.path.exists(f"{froot}_stars.cat"):
-                scat = calibration.generate_star_catalog(fname)
-            else:
-                scat = calibration.read_star_catalog(fname)
 
-            # Calibrate
-            if not os.path.exists(f"{froot}_calib.wcs"):
-                w, rmsx, rmsy, nused, is_calibrated = calibration.calibrate(fname, cfg, acat, scat, wref, tref)
+        # Process files
+        p = mp.Pool(processes=cpu_count)
 
-                # Attempt plate solve
-#                if not is_calibrated and scat.nstars > nstarsmin:
-#                    print(colored(f"Computing astrometric calibration for {fname}", "yellow"))
-#                    wtmp, ttmp = calibration.plate_solve(fname, cfg, calfname)
-                    
-#                    # Retry calibration
-#                    if wtmp is not None:
-#                        wref, tref = wtmp, ttmp
-#                        w, rmsx, rmsy, nused, is_calibrated = calibration.calibrate(fname, cfg, acat, scat, wref, tref)
+        try:
+            chunks = chunk_list(fnames, cpu_count)
+            for chunk in chunks:
+                for result in p.map(process_loop, chunk):
+                    (screenoutput, screenoutput_idents) = result
 
-                # Log output
-                output = f"{os.path.basename(fname)},{w.wcs.crval[0]:.6f},{w.wcs.crval[1]:.6f},{rmsx:.3f},{rmsy:.3f},{nused}/{scat.nstars}"
-                if is_calibrated:
-                    color = "green"
-                else:
-                    color = "red"
-                print(colored(output, color))
+                    if screenoutput is not None:
+                        print(screenoutput)
+                    for screenoutput_ident in screenoutput_idents:
+                        print(screenoutput_ident)
 
-            # Skip if png exists
-            if os.path.exists(f"{froot}_0.png"):
-                continue
-                
-            # Read Fourframe
-            ff = FourFrame(fname, cfg)
-                
-            # Generate predictions
-            predictions = ff.generate_satellite_predictions(cfg)
-
-            # Find tracks
-            if ff.is_calibrated():
-                tracks = ff.find_tracks_by_hough3d(cfg)
-            else:
-                tracks = []
-
-            # Output dictionary
-            output_dict = {"site_id": ff.site_id,
-                           "latitude": ff.lat,
-                           "longitude": ff.lon,
-                           "height": ff.height,
-                           "observer": ff.observer}
-            
-            # Loop over tracks
-            ident_dicts = []
-            obs = []
-            satno = 90000
-            for t in tracks:
-                # Identify
-                ident, is_identified = t.identify(predictions, satno, "22 500A", None, cfg, abbrevs, tlefiles)
-                if not is_identified:
-                    satno += 1
-
-                # Identification dictionary
-                ident_dict = {"satno": ident.satno,
-                              "cospar": ident.cospar,
-                              "tlefile": ident.tlefile,
-                              "catalogname": ident.catalogname}
-
-                # Save track
-                t.save(f"{ff.froot}_{ident.satno:05d}_{ident.catalogname}.csv", ff)
-
-                # Measure single position
-                m = t.measure_single_position(ff)
-                iod_line = m.to_iod_line(ff, ident)
-
-                # Measure multiple position
-                ms = t.measure_multiple_positions(ff)
-                iod_lines = [mt.to_iod_line(ff, ident) for mt in ms]
-
-                # Add to dictionary
-                single_measurement = {"time": m.t.isot,
-                                      "ra": float(m.ra),
-                                      "dec": float(m.dec),
-                                      "drxdt": float(m.drxdt),
-                                      "drydt": float(m.drydt)}
-                multiple_measurements = [{"time": mt.t.isot,
-                                          "ra": float(mt.ra),
-                                          "dec": float(mt.dec),
-                                          "drxdt": float(mt.drxdt),
-                                          "drydt": float(mt.drydt)} for mt in ms]
-                ident_dict["measurement"] = single_measurement
-                ident_dict["iod_line"] = iod_line
-                ident_dict["measurements"] = multiple_measurements
-                ident_dict["iod_lines"] = [line for line in iod_lines]
-                ident_dicts.append(ident_dict)
-
-                # Store observation
-                obs.append(Observation(ident.satno, ident.catalogname, iod_line, iod_lines))
-
-                
-            # Store output
-            if ident_dicts is not []:
-                output_dict["satellites"] = ident_dicts
-
-                with open(f"{ff.froot}_data.yaml", "w") as fp:
-                    yaml.dump(output_dict, fp, sort_keys=False)
-                
-            # Write observations
-            for o in obs:
-                # Open file
-                outfname = f"{ff.froot}_{o.satno:05d}_{o.catalogname}.dat"
-                with open(outfname, "w") as fp:
-                    fp.write(f"{o.iod_line}\n")
-                outfname = f"{ff.froot}_{o.satno:05d}_{o.catalogname}_m.dat"
-                with open(outfname, "w") as fp:
-                    for iod_line in o.iod_lines:
-                        fp.write(f"{iod_line}\n")
-                if o.catalogname == "catalog":
-                    color = "grey"
-                elif o.catalogname == "classfd":
-                    color = "blue"
-                elif o.catalogname == "unid":
-                    color = "magenta"
-                print(colored(o.iod_line, color))
-
-            # Generate plots
-            ff.diagnostic_plot(predictions, None, None, cfg)
-            for track, o in zip(tracks, obs):
-                ff.diagnostic_plot(predictions, track, o, cfg)
-
-#        # Get files
-#        fitsfnames = sorted(glob.glob(os.path.join(args.file_dir, "2*.fits")))
-#        procfnames = sorted(glob.glob(os.path.join(args.file_dir, "2*.fits.png")))
-#        fnames = [fname for fname in fitsfnames if f"{fname}.png" not in procfnames]
+            p.close()
+            p.join()
+        except KeyboardInterrupt:
+            p.close()
+            p.join()
 
         # Sleep
         try:
